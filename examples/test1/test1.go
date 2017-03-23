a5539f5b385334c5446ec1d0a69852c46004a7c0
Camilo Viecco <cviecco@gmail.com>
2017-03-23 10:43:13 -0700

    simple entry example

--- examples/test1/test1.go
+++ examples/test1/test1.go
@@ -1,55 +1,17 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-
-	"gopkg.in/yaml.v2"
-
-	"golang.org/x/net/context"
 
 	"github.com/cviecco/go-simple-oidc-auth/authhandler"
 )
 
 var (
 	configFilename = flag.String("config", "config.yml", "The filename of the configuration")
-	debug          = flag.Bool("debug", true, "Enable debug messages to console")
 )
-
-type OidcConfig struct {
-	ClientID     string
-	ClientSecret string
-	ProviderURL  string
-}
-
-type AppConfigFile struct {
-	Openidc OidcConfig
-}
-
-func loadVerifyConfigFile(configFilename string) (AppConfigFile, error) {
-	var config AppConfigFile
-	if _, err := os.Stat(configFilename); os.IsNotExist(err) {
-		err = errors.New("mising config file failure")
-		return config, err
-	}
-	source, err := ioutil.ReadFile(configFilename)
-	if err != nil {
-		//panic(err)
-		err = errors.New("cannot read config file")
-		return config, err
-	}
-	err = yaml.Unmarshal(source, &config)
-	if err != nil {
-		err = errors.New("Cannot parse config file")
-		return config, err
-	}
-	return config, nil
-}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
@@ -58,20 +20,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	flag.Parse()
 
-	config, err := loadVerifyConfigFile(*configFilename)
+	// if you alresy use the context:
+	//ctx := context.Background()
+	// simpleOidcAuth, err := authhandler.NewSimpleOIDCAuthFromConfig(configFilename, &ctx)
+	simpleOidcAuth, err := authhandler.NewSimpleOIDCAuthFromConfig(configFilename, nil)
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("appConfig: %+v\n", config)
 
-	////
-	ctx := context.Background()
-	simpleOidcAuth := authhandler.NewSimpleOIDCAuth(&ctx, config.Openidc.ClientID, config.Openidc.ClientSecret, config.Openidc.ProviderURL)
+	// The original code was just:
+	// http.HandleFunc("/", handler)
+	// Now we first make it into a handler and use this as
+	//finalHandler := http.HandlerFunc(handler)
+	http.Handle("/", simpleOidcAuth.Handler(http.HandlerFunc(handler)))
 
-	//http.HandleFunc("/", handler)
-	finalHandler := http.HandlerFunc(handler)
-	//http.Handle("/", OidcAuthState.Handler(finalHandler))
-	http.Handle("/", simpleOidcAuth.Handler(finalHandler))
 	log.Printf("listening on http://%s/", "127.0.0.1:5556")
 	log.Fatal(http.ListenAndServe("127.0.0.1:5556", nil))
 }
